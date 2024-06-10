1099650eab351b7948ee122fd652127b37879439
enchufin <lincografic@gmail.com>
2024-06-10 13:33:39 +0200

    ColorSwichApp según el ejercicio de react.dev

---
+++ src/exercises/ColorSwitchApp.jsx
@@ -0,0 +1,40 @@
+import {useState} from 'react';
+/* Componente ColorButton */
+function ColorButton({ cambioColor }){
+return(
+    <button onClick=
+    {e=>{e.stopPropagation(); 
+         cambioColor();}
+    }> Cambio de color </button>
+);}
+/*  fin de componente  */
+
+function getRandomLightColor() {
+    let r = 150 + Math.round(100 * Math.random());
+    let g = 150 + Math.round(100 * Math.random());
+    let b = 150 + Math.round(100 * Math.random());
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+/* Manejadores */
+function handleCambioColor(){
+    let sectionStyle = document.getElementById('colorContainer').style;
+    sectionStyle.backgroundColor = getRandomLightColor();
+}
+
+export default function ColorSwitchApp() {
+  return (
+    <>
+     <div id='colorContainer'>
+        <h1>Cambio de Color Random</h1>
+       
+            <ColorButton cambioColor={handleCambioColor}/>
+            <p>
+                <strong> Clicks fuera del boton </strong>
+            </p>
+
+        </div>
+    </>
+    
+  )
+}
--- src/sections/RespondingEvents.jsx
+++ src/sections/RespondingEvents.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ListaDeBotones from '../exercises/ListaDeBotones'
+import ColorSwitchApp from '../exercises/ColorSwitchApp'
 
 
 export default function RespondingEvents() {
   return (
     <>
      <ListaDeBotones/>
+     <ColorSwitchApp />
     </>
    
   )
